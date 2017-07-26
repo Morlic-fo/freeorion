533c52e6c9e006bb99653d335d3726a620ba67c3
Morlic-fo <Morlic@gmx.de>
2017-07-27 00:37:55 +0200

    Code cleanup according to PEP8 and simplify logic

--- default/python/common/charting/plot_dumped_graph.py
+++ default/python/common/charting/plot_dumped_graph.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from matplotlib.legend_handler import HandlerPatch
 
+
 class HandlerCircle(HandlerPatch):
     def create_artists(self, legend, orig_handle,
                        xdescent, ydescent, width, height, fontsize, trans):
@@ -18,6 +19,7 @@ class HandlerCircle(HandlerPatch):
         self.update_prop(p, orig_handle, legend)
         p.set_transform(trans)
         return [p]
+
 
 def parse_file(file_name):
     print "processing file ", file_name
@@ -64,52 +66,56 @@ color_map = OrderedDict([('Home', '#00008B'),
                          ('Offensive System', '#DC143C'),
                          ('Other Enemy System', '#F78181'),
                          ])
-color_name_lookup = OrderedDict([(v,k) for k,v in color_map.items()])
+color_name_lookup = OrderedDict([(tag, color) for color, tag in color_map.items()])
 
-def draw(G, empire_id):
-    edges = [(u, v) for (u, v) in G.edges()]
 
-    pos = {n: (data['pos'][0], -data['pos'][1]) for n, data in G.nodes(data=True)}  # positions for all nodes
+def draw(g, empire_id):
+    edges = [(u, v) for (u, v) in g.edges()]
+
+    pos = {n: (data['pos'][0], -data['pos'][1]) for n, data in g.nodes(data=True)}  # positions for all nodes
 
     def get_color(data_dict):
         if data_dict.get('home_system', False):
             return color_map['Home']
-        if not data_dict.get('explored', False):
-            return color_map['Unexplored']
-        elif not data_dict.get('owners', []):
-            if data_dict.get('border_system', False):
-                return color_map['Unowned Border System']
-            elif data_dict.get('expansion_system', False):
-                return color_map['Expansion System']
-            else:
-                return color_map['Misc']
-        elif empire_id in data_dict.get('owners', []):
-            if data_dict.get('border_system', False):
+
+        if data_dict.get('border_system', False):
+            if empire_id in data_dict.get('owners', []):
                 return color_map['Own Border Colony']
             else:
+                return color_map['Unowned Border System']
+
+        if data_dict.get('offensive_system', False):
+            return color_map['Offensive System']
+
+        if data_dict.get('owners', []):
+            if empire_id in data_dict.get('owners', []):
                 return color_map['Own Interior Colony']
-        else:
-            if data_dict.get('offensive_system', False):
-                return color_map['Offensive System']
             else:
                 return color_map['Other Enemy System']
 
-    node_colors = [get_color(data) for n, data in G.nodes(data=True)]
-    nx.draw_networkx_nodes(G, pos, node_size=100, node_color=node_colors,
-                           )
+        if data_dict.get('expansion_system', False):
+            return color_map['Expansion System']
+
+        if not data_dict.get('explored', False):
+            return color_map['Unexplored']
+
+        return color_map['Misc']
+
+    node_colors = [get_color(data) for n, data in g.nodes(data=True)]
+    nx.draw_networkx_nodes(g, pos, node_size=100, node_color=node_colors)
     # edges
-    nx.draw_networkx_edges(G, pos, edgelist=edges, width=1, alpha=0.5, edge_color='b', style='dashed')
+    nx.draw_networkx_edges(g, pos, edgelist=edges, width=1, alpha=0.5, edge_color='b', style='dashed')
     # labels
     pos = {k: (a, b - 15) for k, (a, b) in pos.items()}
-    nx.draw_networkx_labels(G, pos, font_size=10, font_family='DejaVu Sans', labels={n: unicode(data['name'], 'utf-8') for n, data in G.nodes(data=True)},
-                            )
+    nx.draw_networkx_labels(g, pos, font_size=10, font_family='DejaVu Sans',
+                            labels={n: unicode(data['name'], 'utf-8') for n, data in g.nodes(data=True)})
     plt.axis('off')
     colors_present = [_c for _c in color_name_lookup if _c in node_colors]
-    legend_symbols = [mpatches.Circle((1,1), 1, facecolor = _c, edgecolor="black") for _c in colors_present]
+    legend_symbols = [mpatches.Circle((1, 1), 1, facecolor=_c, edgecolor="black") for _c in colors_present]
     legend_labels = [color_name_lookup[_c] for _c in colors_present]
     plt.legend(legend_symbols, legend_labels, handler_map={mpatches.Circle: HandlerCircle()})
     mng = plt.get_current_fig_manager()
-    mng.resize(1200,1000)
+    mng.resize(1200, 1000)
     # plt.savefig("universe.png")  # save as png
     plt.show(block=True)
 
@@ -117,18 +123,19 @@ def draw(G, empire_id):
 def main():
     if os.name == 'nt':
         home = os.path.expanduser("~")
-        dataDir = home + "\\AppData\\Roaming\\FreeOrion"
+        data_dir = home + "\\AppData\\Roaming\\FreeOrion"
+    elif os.name == 'posix':
+        data_dir = (os.environ.get('XDG_DATA_HOME', os.environ.get('HOME', "") + "/.local/share") + "/freeorion")
     else:
-        dataDir = (os.environ.get('HOME', "") + "/.freeorion") if os.name != 'posix' else (os.environ.get('XDG_DATA_HOME', os.environ.get('HOME', "") + "/.local/share") + "/freeorion")
-    
-    print "Starting script"
-    logfiles = sorted(glob(dataDir + os.sep + "A*.log"))
-    A1log = glob(dataDir + os.sep + "AI_1.log")
-    if A1log and A1log[0] in logfiles:
-        A1Time = os.path.getmtime(A1log[0])
+        data_dir = (os.environ.get('HOME', "") + "/.freeorion")
+
+    logfiles = sorted(glob(data_dir + os.sep + "A*.log"))
+    log_ai1 = glob(data_dir + os.sep + "AI_1.log")
+    if log_ai1 and log_ai1[0] in logfiles:
+        time_ai1 = os.path.getmtime(log_ai1[0])
         for path in logfiles[::-1]:
             logtime = os.path.getmtime(path)
-            if logtime < A1Time - 300:
+            if logtime < time_ai1 - 300:
                 del logfiles[logfiles.index(path)]
                 print "skipping stale logfile ", path
     for lfile in logfiles:
@@ -136,7 +143,7 @@ def main():
             g, empire_id = parse_file(lfile)
             if len(g.nodes()) > 1:
                 draw(g, empire_id)
-        except Exception as e:
+        except:
             print >> sys.stderr, "Couldn't parse file:"
             print >> sys.stderr, traceback.format_exc()
 
