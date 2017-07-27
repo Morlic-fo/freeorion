938edfa75b88180e97509068452b85890910efef
Morlic-fo <Morlic@gmx.de>
2017-07-27 21:59:08 +0200

    Add filtering of shown markers

--- default/python/common/charting/plot_dumped_graph.py
+++ default/python/common/charting/plot_dumped_graph.py
@@ -8,6 +8,7 @@ import traceback
 import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+from matplotlib.widgets import CheckButtons
 from matplotlib.legend_handler import HandlerPatch
 
 
@@ -57,7 +58,7 @@ def parse_file(file_name):
     return g, empire_id
 
 color_map = OrderedDict([('Home', '#00008B'),
-                         ('Own Interior Colony', '#4169E1'),
+                         ('Own Colony', '#4169E1'),
                          ('Own Border Colony', '#3ADF00'),
                          ('Unowned Border System', '#FFFF00'),
                          ('Expansion System', '#F2F5A9'),
@@ -71,30 +72,35 @@ color_name_lookup = OrderedDict([(tag, color) for color, tag in color_map.items(
 
 
 def draw(g, empire_id):
-    edges = [(u, v) for (u, v) in g.edges()]
+    plot_selection = {'Border systems': True, 'Expansion systems': False, 'Offensive Systems': False}
 
+    edges = [(u, v) for (u, v) in g.edges()]
     pos = {n: (data['pos'][0], -data['pos'][1]) for n, data in g.nodes(data=True)}  # positions for all nodes
+    pos = {k: (a, b - 15) for k, (a, b) in pos.items()}
+
+    ax_graph = plt.axes()
 
     def get_color(data_dict):
         if data_dict.get('home_system', False):
             return color_map['Home']
 
-        if data_dict.get('border_system', False):
+        if data_dict.get('border_system', False) and plot_selection['Border systems']:
             if empire_id in data_dict.get('owners', []):
                 return color_map['Own Border Colony']
             else:
                 return color_map['Unowned Border System']
 
-        if data_dict.get('offensive_system', False):
+        if data_dict.get('offensive_system', False) and plot_selection['Offensive Systems']:
+            print plot_selection
             return color_map['Offensive System']
 
         if data_dict.get('owners', []):
             if empire_id in data_dict.get('owners', []):
-                return color_map['Own Interior Colony']
+                return color_map['Own Colony']
             else:
                 return color_map['Other Enemy System']
 
-        if data_dict.get('expansion_system', False):
+        if data_dict.get('expansion_system', False) and plot_selection['Expansion systems']:
             return color_map['Expansion System']
 
         if not data_dict.get('explored', False):
@@ -105,19 +111,39 @@ def draw(g, empire_id):
 
         return color_map['Misc']
 
-    node_colors = [get_color(data) for n, data in g.nodes(data=True)]
-    nx.draw_networkx_nodes(g, pos, node_size=100, node_color=node_colors)
-    # edges
-    nx.draw_networkx_edges(g, pos, edgelist=edges, width=1, alpha=0.5, edge_color='b', style='dashed')
-    # labels
-    pos = {k: (a, b - 15) for k, (a, b) in pos.items()}
-    nx.draw_networkx_labels(g, pos, font_size=10, font_family='DejaVu Sans',
-                            labels={n: unicode(data['name'], 'utf-8') for n, data in g.nodes(data=True)})
-    plt.axis('off')
-    colors_present = [_c for _c in color_name_lookup if _c in node_colors]
-    legend_symbols = [mpatches.Circle((1, 1), 1, facecolor=_c, edgecolor="black") for _c in colors_present]
-    legend_labels = [color_name_lookup[_c] for _c in colors_present]
-    plt.legend(legend_symbols, legend_labels, handler_map={mpatches.Circle: HandlerCircle()})
+    def draw_graph():
+        plt.sca(ax_graph)
+        ax_graph.clear()
+        print plot_selection
+        node_colors = [get_color(data) for n, data in g.nodes(data=True)]
+        print node_colors
+        nx.draw_networkx_nodes(g, pos, ax=ax_graph, node_size=100, node_color=node_colors)
+        colors_present = [_c for _c in color_name_lookup if _c in node_colors]
+        legend_symbols = [mpatches.Circle((1, 1), 1, facecolor=_c, edgecolor="black") for _c in colors_present]
+        legend_labels = [color_name_lookup[_c] for _c in colors_present]
+        plt.legend(legend_symbols, legend_labels, handler_map={mpatches.Circle: HandlerCircle()})
+        nx.draw_networkx_edges(g, pos, ax=ax_graph, edgelist=edges, width=1, alpha=0.5, edge_color='b', style='dashed')
+        nx.draw_networkx_labels(g, pos, ax=ax_graph, font_size=10, font_family='DejaVu Sans',
+                                labels={n: unicode(data['name'], 'utf-8') for n, data in g.nodes(data=True)})
+        plt.axis('off')
+        plt.gcf().canvas.draw()
+
+    draw_graph()
+
+    def selection_changed_callback_fcn(label):
+        plot_selection[label] = not plot_selection[label]
+        draw_graph()
+
+    # add checkboxes to limit the shown data
+    rax = plt.axes([0.02, 0.4, 0.2, 0.15])
+    selection_checkboxes = CheckButtons(
+            rax, ('Border systems', 'Expansion systems', 'Offensive Systems'),
+            (
+                plot_selection['Border systems'], plot_selection['Expansion systems'],
+                plot_selection['Offensive Systems']))
+
+    selection_checkboxes.on_clicked(selection_changed_callback_fcn)
+
     mng = plt.get_current_fig_manager()
     mng.resize(1200, 1000)
     # plt.savefig("universe.png")  # save as png
